718479bb384a4ba8c20224b8dc8c6bdd648acf86
Dmitrii Kilishek <mentalclear@gmail.com>
2022-05-28 14:32:23 -0400

    Unit 2 complete All assignments

--- 02-Unit/Assignments/basics-assignment-3-problem/app.js
+++ 02-Unit/Assignments/basics-assignment-3-problem/app.js
@@ -1,8 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
-
+            number: 0,
         };
+    },
+    computed: {
+        result() {
+            if (this.number < 37) {
+                return 'Not there yet!';
+            } else if (this.number === 37) {
+                return this.number;
+            } else {
+                return 'Too much!';
+            }
+        }
+    },
+    methods: {
+        addNum(num) {
+            this.number += num;
+        },
+    },
+    watch: {
+        result() {
+            setTimeout(() => { this.number = 0; }, 5000);
+        },
     },
 });
 
