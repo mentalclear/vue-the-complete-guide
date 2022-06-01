f708697b2663df945b105ee7e1b042b0618ef979
Dmitrii Kilishek <mentalclear@gmail.com>
2022-06-01 10:30:19 -0400

    Continue Chapt 8 with Demo: Adding More Component Communication

---
+++ new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,48 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for */ -->
+<template>
+  <form @submit.prevent="addNewFriend">
+    <div>
+      <label> Name </label>
+      <input
+        v-model="name"
+        type="text"
+      >
+    </div>
+    <div>
+      <label>Phone </label>
+      <input
+        v-model="phone"
+        type="text"
+      >
+    </div>
+    <div>
+      <label>Email </label>
+      <input
+        v-model="email"
+        type="text"
+      >
+    </div>
+    <button>Add Contact</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-new-friend'],
+  data() {
+    return {
+      name: '',
+      phone: '',
+      email: '',
+    };
+  },
+  methods: {
+    addNewFriend() {
+      this.$emit('add-new-friend', this.name, this.phone, this.email);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
