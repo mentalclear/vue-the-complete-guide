73d31386af719212ff8be46672307dd4501633f8
Dmitrii Kilishek <mentalclear@gmail.com>
2022-06-03 07:28:16 -0400

    Continue Chapt 10 - Dynamic Components and the Fallthrough Attribute

--- 10-Chapter/course-project/src/App.vue
+++ 10-Chapter/course-project/src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <section />
+  <TheHeader :title="'RememberMe'" />
+  <StoredResources :resources="storedResources" />
 </template>
 
 <script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
 export default {
+  components: {
+    StoredResources,
+    TheHeader,
+  },
   data() {
     return {
-
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google stuff',
+          link: 'https://google.com',
+        },
+      ],
     };
   },
 };
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
