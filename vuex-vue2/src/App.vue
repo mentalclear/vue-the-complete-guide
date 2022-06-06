e5fe78ccd2f1f6ab53ccf03692109f5ea5d6b601
Dmitrii Kilishek <mentalclear@gmail.com>
2022-06-06 08:05:24 -0400

    Cleaned up for working counter

--- vuex-vue2/src/App.vue
+++ vuex-vue2/src/App.vue
@@ -3,9 +3,9 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Vuex</h1>
-        <app-result :counter="counter" />
+        <app-result />
         <hr>
-        <app-counter @updated="counter += $event" />
+        <app-counter />
       </div>
     </div>
   </div>
@@ -19,11 +19,6 @@ export default {
   components: {
     appCounter: TheCounter,
     appResult: TheResult,
-  },
-  data() {
-    return {
-      counter: 0,
-    };
   },
 };
 </script>
