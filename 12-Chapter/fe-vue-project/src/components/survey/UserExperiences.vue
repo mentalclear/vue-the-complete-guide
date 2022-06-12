9c65b95c9a8807beb68c4fd59c004ee5ff354c57
Dmitrii Kilishek <mentalclear@gmail.com>
2022-06-12 09:38:56 -0400

    TODO: implement loadExperiences() with axios, then Chapte 13

--- 12-Chapter/fe-vue-project/src/components/survey/UserExperiences.vue
+++ 12-Chapter/fe-vue-project/src/components/survey/UserExperiences.vue
@@ -7,7 +7,18 @@
           Load Submitted Experiences
         </base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">
+        Loading...
+      </p>
+      <p v-else-if="!isLoading && error">
+        {{ error }}
+      </p>
+      <p
+        v-else-if="!isLoading && (!results || results.length === 0)"
+      >
+        No stored experiences found. Start adding some survey results first.
+      </p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -18,10 +29,11 @@
     </base-card>
   </section>
 </template>
-
 <script>
 import axios from 'axios';
 import SurveyResult from './SurveyResult.vue';
+
+const url = 'https://vue-http-demo-7b2c7-default-rtdb.firebaseio.com/surveys.json';
 
 export default {
   components: {
@@ -30,23 +42,63 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
+      error: null,
     };
   },
+  mounted() {
+    this.loadExperiences();
+  },
   methods: {
-    loadExperiences() {
-      axios.get('https://vue-http-demo-7b2c7-default-rtdb.firebaseio.com/surveys.json')
+    // @TODO: Try implementing this method to work like loadExperiences()
+    loadExperiencesAxios() {
+      this.isLoading = true;
+      axios.get(url)
         .then((response) => {
           if (response.ok) {
             return response.json();
           }
           return response;
         }).then((output) => {
+          this.isLoading = false;
           const results = [];
-          // With current Firebase .data must be used.
+          // Axios retuns much more than just the 'data' contents.
+          // So here we need to filter out the data
           Object.keys(output.data).forEach((id) => {
             results.push({ id, name: output.data[id].name, rating: output.data[id].rating });
           });
           this.results = results;
+        });
+    },
+
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch(url)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          // Server error handling
+          throw new Error('Could not get data!');
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          if (data !== null) {
+            Object.keys(data).forEach((id) => {
+              results.push({ id, name: data[id].name, rating: data[id].rating });
+            });
+            this.results = results;
+          } else {
+            this.results = results;
+          }
+        })
+        .catch((error) => {
+          // Technical error handling
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later.';
         });
     },
   },
